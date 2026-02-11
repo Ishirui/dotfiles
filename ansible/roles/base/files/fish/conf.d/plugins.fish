c1c9716747ccdce45fd7cb9b8f60194a570e813b
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2026-02-12 00:55:28 +0100

    feat: add some fish plugins

---
+++ ansible/roles/base/files/fish/conf.d/plugins.fish
@@ -0,0 +1,2 @@
+fzf_configure_bindings
+set -u sponge_purge_only_one_exit true
--- ansible/roles/base/files/fish/config.fish
+++
@@ -1,7 +0,0 @@
-source
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
