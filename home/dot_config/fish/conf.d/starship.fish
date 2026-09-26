62496be58f77bc466e6e657e26208c26eba16b10
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2026-09-26 19:35:20 +0200

    fix: don't use starship path incompatible with macOS

--- home/dot_config/fish/conf.d/starship.fish
+++ home/dot_config/fish/conf.d/starship.fish
@@ -1,5 +1,5 @@
 set -q STARSHIP_CONFIG; or set -Ux STARSHIP_CONFIG ~/.config/fish/starship.toml
 
 if status is-interactive
-    source (/usr/bin/starship init fish --print-full-init | psub)
+    source (starship init fish --print-full-init | psub)
 end
